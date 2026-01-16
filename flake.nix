d05126b39e9eb72d3ba34a90fdfc79bdf362231c
ReimersS <sebastian.reimers@tum.de>
2026-01-16 10:17:52 +0000

    Adding a nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.maven.buildMavenPackage rec {
+          pname = "Dat3m";
+          name = "dartagnan";
+          src = self;
+          version = "4.3.0";
+
+          nativeBuildInputs = with pkgs; [ makeWrapper ];
+          mvnParameters = "-DskipTests";
+
+          patches = [ ./git-id.patch ];
+
+          mvnHash = "sha256-uRgAqglDrOkLMtWI1Bleefo6lDiotIj9lb31px1zTB4=";
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r dartagnan $out/
+
+            makeWrapper ${pkgs.jdk25_headless}/bin/java $out/bin/${name} \
+              --add-flags "-jar $out/dartagnan/target/${name}.jar"
+          '';
+        };
+      }
+    );
+}
