e5f26dbdc94a3c1d20e35b3be37bbe79a0e59899
ReimersS <sebastian.reimers@tum.de>
2026-02-20 13:32:06 +0000

    Use correct jdk & let z3 find stdlibs

--- flake.nix
+++ flake.nix
@@ -4,29 +4,36 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
         FixedHashes = {
           x86_64-linux = "sha256-dK0GgR3A//wQ29jRlocWBxPp3o6tdxy2iUf6zTMgV9w=";
           aarch64-linux = "sha256-t837WYusF1ePHgHXY3lmP+wESe/GU6QtjEgNkv4zOy0=";
         };
+        graal-mvn = pkgs.maven.override {
+          jdk_headless = pkgs.graalvmPackages.graalvm-oracle;
+        };
       in
       {
-        packages.default = pkgs.maven.buildMavenPackage rec {
+        packages.default = graal-mvn.buildMavenPackage rec {
           pname = "Dat3m";
           name = "dartagnan";
           src = self;
           version = "4.4.0-alpha";
 
-          nativeBuildInputs = with pkgs; [ makeWrapper jre25_minimal jdk25_headless clang ];
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
+          nativeBuildInputs = with pkgs; [ makeWrapper graalvmPackages.graalvm-oracle clang ];
           mvnParameters = "-DskipTests";
 
           mvnHash = FixedHashes.${system};
+          maven = graal-mvn;
+          mvnJdk = pkgs.graalvmPackages.graalvm-oracle;
 
           installPhase = ''
             mkdir -p $out
             cp -r dartagnan $out/
 
-            makeWrapper ${pkgs.jdk25_headless}/bin/java $out/bin/${name} \
+            makeWrapper ${pkgs.graalvmPackages.graalvm-oracle}/bin/java $out/bin/${name} \
               --add-flags "-jar $out/dartagnan/target/${name}.jar"
           '';
         };
